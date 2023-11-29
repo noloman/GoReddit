ea842d3c508d5e7e6178e017a18260ca93dd6efc
Manuel Lorenzo <manulorenzop@gmail.com>
2023-11-29 11:22:44 +0100

    create DB stores

--- goreddit.go
+++ goreddit.go
@@ -22,3 +22,27 @@ type Comment struct {
 	Content string    `db:"content"`
 	Votes   int       `db:"votes"`
 }
+
+type ThreadStore interface {
+	Thread(id uuid.UUID) (Thread, error)
+	Threads() ([]Thread, error)
+	CreateThread(t *Thread) error
+	UpdateThread(t *Thread) error
+	DeleteThread(id uuid.UUID) error
+}
+
+type PostStore interface {
+	Post(id uuid.UUID) (Post, error)
+	PostsByThread(ThreadID uuid.UUID) ([]Post, error)
+	CreatePost(t *Post) error
+	UpdatePost(t *Post) error
+	DeletePost(id uuid.UUID) error
+}
+
+type CommentStore interface {
+	Comment(id uuid.UUID) (Comment, error)
+	CommentsByPost(PostID uuid.UUID) ([]Comment, error)
+	CreateComment(t *Comment) error
+	UpdateComment(t *Comment) error
+	DeleteComment(id uuid.UUID) error
+}
