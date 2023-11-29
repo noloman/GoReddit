eacd2940b111f57b9b049feae2e9fda42f1f9906
Manuel Lorenzo <manulorenzop@gmail.com>
2023-11-29 11:17:24 +0100

    create initial project structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/noloman/goreddit
+
+go 1.21.4
+
+require github.com/google/uuid v1.4.0 // indirect
---
+++ goreddit.go
@@ -0,0 +1,24 @@
+package goreddit
+
+import "github.com/google/uuid"
+
+type Thread struct {
+	ID          uuid.UUID `db:"id"`
+	Title       string    `db:"title`
+	Description string    `db:"description"`
+}
+
+type Post struct {
+	ID       uuid.UUID `db:"id"`
+	ThreadID uuid.UUID `db:"thread_id"`
+	Title    string    `db:"title"`
+	Content  string    `db:"content"`
+	Votes    int       `db:"votes"`
+}
+
+type Comment struct {
+	ID      uuid.UUID `db:"id"`
+	PostID  uuid.UUID `db:"post_id"`
+	Content string    `db:"content"`
+	Votes   int       `db:"votes"`
+}
